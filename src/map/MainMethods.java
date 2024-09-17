d7b42f7477ced1e003d6a2f36c4699e7c34b1b6a
Rafael Gomes <rafaelsouzagomes13@gmail.com>
2024-09-16 23:32:44 -0300

    map

---
+++ src/map/MainMethods.java
@@ -0,0 +1,49 @@
+package map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainMethods {
+
+	public static void main(String[] args) {
+//		compute();
+//		putIfAbsent();
+		
+		Map<Integer, Integer> map = new HashMap<>();
+//		map.put(1, null)
+		
+		int result = map.getOrDefault(1, -1);
+		System.out.println(result);
+		
+		map.put(1, 1000);
+		result = map.getOrDefault(1, -1);
+		System.out.println(result);
+		
+	}
+
+	private static void putIfAbsent() {
+		Map<Character, Integer> map = new HashMap<>();
+		char key = 'D';
+		
+		map.putIfAbsent(key, 4);
+		System.out.println(map.get(key));
+		
+		map.putIfAbsent(key, 5);
+		System.out.println(map.get(key));
+	}
+
+	private static void compute() {
+		Map<Integer, Integer> map = new HashMap<>();
+		
+		
+		map.compute(1, (key, value) -> value==null?1:value+1);
+		map.compute(2, (key, value) -> value==null?1:value+1);
+		map.compute(3, (key, value) -> value==null?1:value+1);
+		map.compute(3, (key, value) -> value==null?1:value+1);
+		map.compute(3, (key, value) -> value==null?1:value+1);
+
+		for(Map.Entry<Integer,Integer> entry: map.entrySet()) 
+			System.out.println(entry.getKey() + " " + entry.getValue());
+	}
+	
+}
